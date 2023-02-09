276288a4d32857dd3dc88b1e0844f1de94a03a5a
kireev <kireevmw@yandex.ru>
2023-02-09 14:37:08 +0700

    header in process

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,10 +1,123 @@
 <template>
-  <header class="header">header</header>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="https://sibdev.pro/">
+        <img
+          class="header__logo-img"
+          src="@/assets/img/sibdev-logo.svg"
+          alt="logo"
+        />
+      </a>
+      <div class="nav-bar">
+        <div class="nav-bar__inner">
+          <div class="nav-bar__items">
+            <div
+              v-for="(item, index) of links"
+              :key="item.title"
+              class="nav-bar__item"
+              @click="setTabIndex(index)"
+            >
+              <router-link :to="item.to">
+                {{ item.title }}
+              </router-link>
+            </div>
+            <div class="nav-bar__shape" :style="currentLinkBorderPosition" />
+          </div>
+        </div>
+      </div>
+      <BaseButton :mode="'text'">Войти</BaseButton>
+    </div>
+  </header>
 </template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const tabIndex = ref(0);
+
+const links = [
+  {
+    title: "Поиск",
+    to: "/",
+  },
+  {
+    title: "Избранное",
+    to: "/ui-kit",
+  },
+];
+
+const currentLinkBorderPosition = computed(() => {
+  return `left: ${tabIndex.value * 50}%;`;
+});
+
+const setTabIndex = (index) => {
+  tabIndex.value = index;
+};
+</script>
 
 <style lang="scss" scoped>
 .header {
-  text-align: center;
-  border-bottom: 1px solid gray;
+  background: #ffffff;
+  border: 1px solid #f1f1f1;
+  &__inner {
+    @include container;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .nav-bar {
+    width: 100%;
+    background: transparent;
+    &__inner {
+      @include container;
+      margin: 0 auto;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      overflow: auto;
+      &::-webkit-scrollbar {
+        width: 0;
+        height: 0;
+        background-color: transparent;
+      }
+      scrollbar-color: transparent transparent;
+      scrollbar-width: none;
+    }
+    &__items {
+      position: relative;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      justify-items: center;
+      grid-gap: 5px;
+    }
+    &__item {
+      padding: 28px 0;
+      color: #1390e5;
+      cursor: pointer;
+      transition: 0.2s ease-in-out;
+      a {
+        display: block;
+        color: #1390e5;
+        text-decoration: none;
+        transition: 0.2s ease-in-out;
+        word-break: break-all;
+      }
+      &:hover a {
+        color: #e1e4ed;
+      }
+    }
+    &__shape {
+      $tabsCount: 2;
+      $pagePadding: 10px;
+      margin: 0 $pagePadding;
+      position: absolute;
+      height: 2px;
+      left: 0;
+      bottom: 0;
+      width: calc((100% / #{$tabsCount}) - #{$pagePadding * 2});
+      background: #1390e5;
+      transition: 0.3s ease-in-out;
+    }
+  }
 }
 </style>
