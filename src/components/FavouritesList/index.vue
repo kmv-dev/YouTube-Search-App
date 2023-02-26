48650c4d951452ca07791be519e4c36092d64d92
kireev <kireevmw@yandex.ru>
2023-02-26 21:37:47 +0700

    add render saved requests list

---
+++ src/components/FavouritesList/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="favourite-list">
+    <div
+      v-for="item in savedRequests"
+      class="favourite-list__item item"
+      :key="item.searchValue"
+    >
+      <span class="item__title">{{ item.requestName }}</span>
+      <div class="item__action">
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  savedRequests: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.favourite-list {
+  margin-bottom: 50px;
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 20px;
+    background: #ffffff;
+    border-bottom: 1px solid #f1f1f1;
+    transition: 0.2s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      background: rgba(197, 228, 249, 0.3);
+    }
+    &:first-child {
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+    }
+    &:last-child {
+      border-bottom-left-radius: 10px;
+      border-bottom-right-radius: 10px;
+    }
+    &__title {
+      font-weight: 500;
+      font-size: 16px;
+    }
+  }
+}
+</style>
--- src/views/favourites.vue
+++ src/views/favourites.vue
@@ -2,9 +2,24 @@
   <div class="favourites">
     <div class="favourites__inner">
       <h1 class="favourites__title">Избранное</h1>
+      <FavouritesList :savedRequests="requests" />
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import FavouritesList from "../components/FavouritesList/index.vue";
+
+const requests = ref([]);
+
+onMounted(async () => {
+  requests.value = await JSON.parse(localStorage.getItem("saveRequests"));
+  requests.value = requests.value.filter(
+    (el) => el.email === localStorage.getItem("userEmail")
+  );
+});
+</script>
 
 <style lang="scss" scoped>
 .favourites {
@@ -14,7 +29,6 @@
   &__title {
     font-weight: 400;
     font-size: 28px;
-    color: #000000;
     margin: 40px 0;
   }
 }
